630c21771085563305a8926d8f1640d4203e8574
roshan-42 <roshanacharya207@gmail.com>
2024-04-26 08:50:34 +0545

    portfolio upgraded

--- app/components/Body/Works.tsx
+++ app/components/Body/Works.tsx
@@ -7,22 +7,28 @@ import Image from "next/image";
 
 const Works = () => {
   return (
-    <div className="flex flex-wrap gap-5 items-center justify-center lg:justify-start">
-      <div className="bg-gray-300 shadow-lg hover:scale-110 transition-all w-fit h-fit rounded-md ">
-        <Link target="blank" href="https://www.laliguras.de/">
-          <div className="w-64 sm:w-96 ">
-            <Image src={work1} alt="" />
-            <p className="p-2 text-gray-600">Laliguras GmbH </p>
-          </div>
-        </Link>
+    <div className="flex flex-col gap-5">
+      <div className="flex items-center gap-3">
+        <p className="font-bold text-3xl">Works</p>
+        <div className="bg-red-500 mt-3 h-[2px] w-24 rounded-full"></div>
       </div>
-      <div className="bg-gray-300 w-fit h-fit rounded-md shadow-lg ">
-        <Link target="black" href="https://ecommerce-fawn-five.vercel.app/">
-          <div className="w-64 sm:w-96 ">
-            <Image src={work2} alt="" />
-            <p className="text-gray-600 p-2">Ecommerce Website </p>
-          </div>
-        </Link>
+      <div className="flex flex-wrap gap-5 items-center justify-center lg:justify-start">
+        <div className="hover:bg-green-500 hover:text-white text-black shadow-lg hover:scale-110 transition-all w-fit h-fit rounded-md ">
+          <Link target="blank" href="https://www.laliguras.de/">
+            <div className="w-64 sm:w-96 ">
+              <Image src={work1} alt="" />
+              <p className="p-2 ">Laliguras GmbH </p>
+            </div>
+          </Link>
+        </div>
+        <div className="hover:bg-orange-500 hover:text-white text-black w-fit h-fit rounded-md shadow-lg hover:scale-110 transition-all">
+          <Link target="black" href="https://ecommerce-fawn-five.vercel.app/">
+            <div className="w-64 sm:w-96 ">
+              <Image src={work2} alt="" />
+              <p className=" p-2">Ecommerce Website </p>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
