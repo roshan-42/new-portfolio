c6e3825a43ccdfbe126626e4a149451d6e13bc4f
roshan-42 <roshanacharya207@gmail.com>
2024-05-12 14:11:19 +0545

    work section fixed

--- app/components/Body/Works.tsx
+++ app/components/Body/Works.tsx
@@ -8,12 +8,12 @@ import Image from "next/image";
 
 const Works = () => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5  h-[25rem]">
       <div className="flex items-center gap-3">
         <p className="font-bold text-3xl">Works</p>
         <div className="bg-red-500 mt-3 h-[2px] w-24 rounded-full"></div>
       </div>
-      <div className="flex flex-wrap gap-5 items-center justify-center lg:justify-start">
+      <div className="flex flex-wrap gap-5 items-center justify-center lg:justify-start py-4">
         <div className="hover:bg-green-500 hover:text-white text-black shadow-lg hover:scale-110 transition-all w-fit h-fit rounded-md ">
           <Link target="blank" href="https://www.laliguras.de/">
             <div className="w-64 sm:w-96 ">
