cbe4fae37bd38fa29ed744fae97d72c618eb5a33
roshan-42 <roshanacharya207@gmail.com>
2024-05-12 12:25:24 +0545

    works updated

--- app/components/Body/Works.tsx
+++ app/components/Body/Works.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import work1 from "@/public/assets/work1.png";
 import work2 from "@/public/assets/work2.png";
+import work3 from "@/public/assets/work3.png";
 
 import Image from "next/image";
 
@@ -26,6 +27,14 @@ const Works = () => {
             <div className="w-64 sm:w-96 ">
               <Image src={work2} alt="" />
               <p className=" p-2">Ecommerce Website </p>
+            </div>
+          </Link>
+        </div>
+        <div className="hover:bg-red-500 hover:text-white text-black w-fit h-fit rounded-md shadow-lg hover:scale-110 transition-all">
+          <Link target="black" href="https://mario-css.vercel.app/">
+            <div className="w-64 sm:w-96 ">
+              <Image src={work3} alt="" />
+              <p className=" p-2">Static Website </p>
             </div>
           </Link>
         </div>
